65fa9851ab884151d55535095dfda8910763d8fc
Jean-Francois Yoteau <jfyoteau@yahoo.fr>
2024-02-12 20:34:35 +0900

    Create ":feature:note" module and move "note" package to this module.

--- feature/note/build.gradle.kts
+++ feature/note/build.gradle.kts
@@ -1,10 +1,11 @@
 import org.jetbrains.compose.ExperimentalComposeLibrary
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.sqldelight)
+    alias(libs.plugins.kotlinSerialization)
 }
 
 kotlin {
@@ -18,16 +19,9 @@ kotlin {
 
     jvm("desktop")
 
-    listOf(
-            iosX64(),
-            iosArm64(),
-            iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
 
     sourceSets {
         val desktopMain by getting
@@ -37,59 +31,51 @@ kotlin {
             implementation(libs.androidx.activity.compose)
             implementation(libs.koin.android)
             implementation(libs.kotlinx.coroutines.android)
+            implementation(libs.sqldelight.driver.android)
             implementation(libs.decompose)
         }
         commonMain.dependencies {
-            implementation(projects.feature.note)
+            implementation(projects.core.databaseSqldelight)
+            implementation(projects.core.decompose)
+            implementation(projects.core.ui)
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
+            implementation(compose.materialIconsExtended)
             implementation(compose.ui)
             @OptIn(ExperimentalComposeLibrary::class)
             implementation(compose.components.resources)
             implementation(libs.koin.core)
             implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.datetime)
+            implementation(libs.sqldelight.coroutines)
             implementation(libs.decompose)
             implementation(libs.decompose.extensions.compose)
+            implementation(libs.kotlinx.serialization.json)
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
+            implementation(libs.sqldelight.driver.sqlite)
             implementation(libs.kotlinx.coroutines.swing)
+        }
+        iosMain.dependencies {
+            implementation(libs.sqldelight.driver.native)
         }
     }
 }
 
 android {
-    namespace = "jfyoteau.noteapp"
+    namespace = "jfyoteau.noteapp.note"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
-
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    sourceSets["main"].res.srcDirs("src/androidMain/res")
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
-
     defaultConfig {
-        applicationId = "jfyoteau.noteapp"
         minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    buildFeatures {
-        compose = true
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        compose = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = libs.versions.androidx.compose.compiler.get() // same as "androidx.compose.compiler:compiler:1.5.9"
@@ -99,18 +85,14 @@ android {
     }
 }
 
-compose.desktop {
-    application {
-        mainClass = "jfyoteau.noteapp.MainKt"
-
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "jfyoteau.noteapp"
-            packageVersion = "1.0.0"
-        }
-    }
-}
-
 compose {
     kotlinCompilerPlugin.set(libs.versions.jetbrains.compose.compiler.get()) // same as "org.jetbrains.compose.compiler:compiler:1.5.8"
+}
+
+sqldelight {
+    databases {
+        create("NoteDatabase") {
+            packageName.set("jfyoteau.noteapp.note.data.database")
+        }
+    }
 }
