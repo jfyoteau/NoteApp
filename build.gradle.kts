953c5ce2756519359973f5967a2905d1c083ad77
Jean-Francois Yoteau <jfyoteau@yahoo.fr>
2024-02-17 17:05:57 +0900

    [common] Add "gradle versions" gradle plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
@@ -5,4 +7,23 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.gradleVersions)
 }
+
+// ==========================================================================================
+// https://github.com/ben-manes/gradle-versions-plugin
+// ==========================================================================================
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
+}
+
+// ==========================================================================================
