13d7799fd9e254b232f60865d5ccd61e7f97023a
Jean-Francois Yoteau <jfyoteau@yahoo.fr>
2024-02-24 15:21:34 +0900

    [gradle] Add "-Xexpect-actual-classes" to kotlin compiler.

--- build-logic/convention/src/main/kotlin/jfyoteau/noteapp/convention/configuration/KotlinMultiplatformConfiguration.kt
+++ build-logic/convention/src/main/kotlin/jfyoteau/noteapp/convention/configuration/KotlinMultiplatformConfiguration.kt
@@ -1,12 +1,20 @@
 package jfyoteau.noteapp.convention.configuration
 
 import jfyoteau.noteapp.convention.AndroidBuildConfig
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
+@OptIn(ExperimentalKotlinGradlePluginApi::class)
 internal fun configureKotlinMultiplatform(
     kotlinMultiplatformExtension: KotlinMultiplatformExtension
 ) {
     kotlinMultiplatformExtension.apply {
+        // https://kotlinlang.org/docs/multiplatform-expect-actual.html#expected-and-actual-classes
+        // To suppress this warning about usage of expected and actual classes
+        compilerOptions {
+            freeCompilerArgs.add("-Xexpect-actual-classes")
+        }
+
         // Android
         androidTarget {
             compilations.all {
